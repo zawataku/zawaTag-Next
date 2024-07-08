ceab4c609195874aa6951d89d02d2f2359b8742d
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-07-08 18:16:22 +0900

    Initial Commit

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div className="navbar bg-slate-100">
+            <div className="navbar-start"></div>
+            <div className="navbar-center">
+                <p className="text-xl">zawaTag</p>
+            </div>
+            <div className="navbar-end"></div>
+        </div>
+    )
+}
